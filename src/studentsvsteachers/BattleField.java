8b440def6114fa2d3d87a5ffbc0a87f1623c82d1
aleahgrym <aleah.grymaloski@brentwood.ca>
2016-03-27 18:23:22 -0700

    new background

--- src/studentsvsteachers/BattleField.java
+++ src/studentsvsteachers/BattleField.java
@@ -6,15 +6,34 @@
 package studentsvsteachers;
 
 import environment.Environment;
+import grid.Grid;
+import images.ResourceTools;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 /**
  *
  * @author Aleah
  */
+
+
 public class BattleField extends Environment{
+    
+    private Grid grid;
+    
+    public BattleField(){
+        
+        grid = new Grid(10, 10, 52, 36, new Point(10, 10), new Color(100, 49, 178, 100));
+
+          BufferedImage temp = (BufferedImage) ResourceTools.loadImageFromResource("items/background.png");
+        this.setBackground(temp.getScaledInstance(1000, 700, Image.SCALE_SMOOTH));
+    }
+           
 
     @Override
     public void initializeEnvironment() {
