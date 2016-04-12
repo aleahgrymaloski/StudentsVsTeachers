6361f37977c98092bbb7751539474e0bb2ceafb7
aleahgrym <aleah.grymaloski@brentwood.ca>
2016-04-12 14:12:19 -0700

    new images!!!

--- src/studentsvsteachers/ZombieImageManager.java
+++ src/studentsvsteachers/ZombieImageManager.java
@@ -22,30 +22,178 @@ public class ZombieImageManager  extends ImageManager {
     public static final String MACINNIS_ZOMBIE_02 = "MACINNIS_ZOMBIE_02";
     public static final String MACINNIS_ZOMBIE_03 = "MACINNIS_ZOMBIE_03";
     public static final String MACINNIS_ZOMBIE_04 = "MACINNIS_ZOMBIE_04";
+    public static final String MACINNIS_ZOMBIE_05 = "MACINNIS_ZOMBIE_05";
+    public static final String MACINNIS_ZOMBIE_06 = "MACINNIS_ZOMBIE_06";
+    public static final String MACINNIS_ZOMBIE_07 = "MACINNIS_ZOMBIE_07";
+    public static final String MACINNIS_ZOMBIE_08 = "MACINNIS_ZOMBIE_08";
+    public static final String MACINNIS_ZOMBIE_09 = "MACINNIS_ZOMBIE_09";
+    public static final String MACINNIS_ZOMBIE_10 = "MACINNIS_ZOMBIE_10";
+    public static final String MACINNIS_ZOMBIE_11 = "MACINNIS_ZOMBIE_11";
+    public static final String MACINNIS_ZOMBIE_12 = "MACINNIS_ZOMBIE_12";
+    public static final String MACINNIS_ZOMBIE_13 = "MACINNIS_ZOMBIE_13";
+    public static final String MACINNIS_ZOMBIE_14 = "MACINNIS_ZOMBIE_14";
+
     //carry on!!!!
 
     public static ArrayList<String> MACINNIS_ZOMBIE_WALK;
 
     
     public static final String LAWRENCE_ZOMBIE_01 = "LAWRENCE_ZOMBIE_01";
-    //carry on...
+    public static final String LAWRENCE_ZOMBIE_02 = "LAWRENCE_ZOMBIE_02";
+    public static final String LAWRENCE_ZOMBIE_03 = "LAWRENCE_ZOMBIE_03";
+    public static final String LAWRENCE_ZOMBIE_04 = "LAWRENCE_ZOMBIE_04";
+    public static final String LAWRENCE_ZOMBIE_05 = "LAWRENCE_ZOMBIE_05";
+    public static final String LAWRENCE_ZOMBIE_06 = "LAWRENCE_ZOMBIE_06";
+    public static final String LAWRENCE_ZOMBIE_07 = "LAWRENCE_ZOMBIE_07";
+    public static final String LAWRENCE_ZOMBIE_08 = "LAWRENCE_ZOMBIE_08";
+    public static final String LAWRENCE_ZOMBIE_09 = "LAWRENCE_ZOMBIE_09";
+    public static final String LAWRENCE_ZOMBIE_10 = "LAWRENCE_ZOMBIE_10";
+    public static final String LAWRENCE_ZOMBIE_11 = "LAWRENCE_ZOMBIE_11";
+    public static final String LAWRENCE_ZOMBIE_12 = "LAWRENCE_ZOMBIE_12";
+    public static final String LAWRENCE_ZOMBIE_13 = "LAWRENCE_ZOMBIE_13";
+    public static final String LAWRENCE_ZOMBIE_14 = "LAWRENCE_ZOMBIE_14";
+    public static final String LAWRENCE_ZOMBIE_15 = "LAWRENCE_ZOMBIE_15";
+    public static final String LAWRENCE_ZOMBIE_16 = "LAWRENCE_ZOMBIE_16";
+    public static final String LAWRENCE_ZOMBIE_17 = "LAWRENCE_ZOMBIE_17";
+    public static final String LAWRENCE_ZOMBIE_18 = "LAWRENCE_ZOMBIE_18";
+    public static final String LAWRENCE_ZOMBIE_19 = "LAWRENCE_ZOMBIE_19";
+    public static final String LAWRENCE_ZOMBIE_20 = "LAWRENCE_ZOMBIE_20";
+    public static final String LAWRENCE_ZOMBIE_21 = "LAWRENCE_ZOMBIE_21";
+    public static final String LAWRENCE_ZOMBIE_22 = "LAWRENCE_ZOMBIE_22";
+    public static final String LAWRENCE_ZOMBIE_23 = "LAWRENCE_ZOMBIE_23";
+    public static final String LAWRENCE_ZOMBIE_24 = "LAWRENCE_ZOMBIE_24";
+    public static final String LAWRENCE_ZOMBIE_25 = "LAWRENCE_ZOMBIE_25";
+    public static final String LAWRENCE_ZOMBIE_26 = "LAWRENCE_ZOMBIE_26";
+    public static final String LAWRENCE_ZOMBIE_27 = "LAWRENCE_ZOMBIE_27";
+    public static final String LAWRENCE_ZOMBIE_28 = "LAWRENCE_ZOMBIE_28";
+    public static final String LAWRENCE_ZOMBIE_29 = "LAWRENCE_ZOMBIE_29";
+    public static final String LAWRENCE_ZOMBIE_30 = "LAWRENCE_ZOMBIE_30";
+
     
     public static ArrayList<String> LAWRENCE_ZOMBIE_WALK;
     
-    
+    public static final String PATEL_ZOMBIE_01 = "PATEL_ZOMBIE_01";
+    public static final String PATEL_ZOMBIE_02 = "PATEL_ZOMBIE_02";
+    public static final String PATEL_ZOMBIE_03 = "PATEL_ZOMBIE_03";
+    public static final String PATEL_ZOMBIE_04 = "PATEL_ZOMBIE_04";
+    public static final String PATEL_ZOMBIE_05 = "PATEL_ZOMBIE_05";
+    public static final String PATEL_ZOMBIE_06 = "PATEL_ZOMBIE_06";
+    public static final String PATEL_ZOMBIE_07 = "PATEL_ZOMBIE_07";
+    public static final String PATEL_ZOMBIE_08 = "PATEL_ZOMBIE_08";
+    public static final String PATEL_ZOMBIE_09 = "PATEL_ZOMBIE_09";
+
+            
+    public static ArrayList<String> PATEL_ZOMBIE_WALK;
+
+
     
     {
         addImage(MACINNIS_ZOMBIE_01, ResourceTools.loadImageFromResource("items/macinnis_1.png"));
-        //carry on!!!!
+        addImage(MACINNIS_ZOMBIE_02, ResourceTools.loadImageFromResource("items/macinnis_2.png"));
+        addImage(MACINNIS_ZOMBIE_03, ResourceTools.loadImageFromResource("items/macinnis_3.png"));
+        addImage(MACINNIS_ZOMBIE_04, ResourceTools.loadImageFromResource("items/macinnis_4.png"));
+        addImage(MACINNIS_ZOMBIE_05, ResourceTools.loadImageFromResource("items/macinnis_5.png"));
+        addImage(MACINNIS_ZOMBIE_06, ResourceTools.loadImageFromResource("items/macinnis_6.png"));
+        addImage(MACINNIS_ZOMBIE_07, ResourceTools.loadImageFromResource("items/macinnis_7.png"));
+        addImage(MACINNIS_ZOMBIE_08, ResourceTools.loadImageFromResource("items/macinnis_8.png"));
+        addImage(MACINNIS_ZOMBIE_09, ResourceTools.loadImageFromResource("items/macinnis_9.png"));
+        addImage(MACINNIS_ZOMBIE_10, ResourceTools.loadImageFromResource("items/macinnis_10.png"));
+        addImage(MACINNIS_ZOMBIE_11, ResourceTools.loadImageFromResource("items/macinnis_11.png"));
+        addImage(MACINNIS_ZOMBIE_12, ResourceTools.loadImageFromResource("items/macinnis_12.png"));
+        addImage(MACINNIS_ZOMBIE_13, ResourceTools.loadImageFromResource("items/macinnis_13.png"));
+        addImage(MACINNIS_ZOMBIE_14, ResourceTools.loadImageFromResource("items/macinnis_14.png"));
+        
+        
+        addImage(LAWRENCE_ZOMBIE_01, ResourceTools.loadImageFromResource("items/lawrence_1.png"));
+        addImage(LAWRENCE_ZOMBIE_02, ResourceTools.loadImageFromResource("items/lawrence_2.png"));
+        addImage(LAWRENCE_ZOMBIE_03, ResourceTools.loadImageFromResource("items/lawrence_3.png"));
+        addImage(LAWRENCE_ZOMBIE_04, ResourceTools.loadImageFromResource("items/lawrence_4.png"));
+        addImage(LAWRENCE_ZOMBIE_05, ResourceTools.loadImageFromResource("items/lawrence_5.png"));
+        addImage(LAWRENCE_ZOMBIE_06, ResourceTools.loadImageFromResource("items/lawrence_6.png"));
+        addImage(LAWRENCE_ZOMBIE_07, ResourceTools.loadImageFromResource("items/lawrence_7.png"));
+        addImage(LAWRENCE_ZOMBIE_08, ResourceTools.loadImageFromResource("items/lawrence_8.png"));
+        addImage(LAWRENCE_ZOMBIE_09, ResourceTools.loadImageFromResource("items/lawrence_9.png"));
+        addImage(LAWRENCE_ZOMBIE_10, ResourceTools.loadImageFromResource("items/lawrence_10.png"));
+                addImage(LAWRENCE_ZOMBIE_10, ResourceTools.loadImageFromResource("items/lawrence_10.png"));
+        addImage(LAWRENCE_ZOMBIE_10, ResourceTools.loadImageFromResource("items/lawrence_10.png"));
+        addImage(LAWRENCE_ZOMBIE_10, ResourceTools.loadImageFromResource("items/lawrence_10.png"));
+        addImage(LAWRENCE_ZOMBIE_10, ResourceTools.loadImageFromResource("items/lawrence_10.png"));
+
+
+        
+
+
+        
+      
+
 
         MACINNIS_ZOMBIE_WALK = new ArrayList<>();
         MACINNIS_ZOMBIE_WALK.add(MACINNIS_ZOMBIE_01);
         MACINNIS_ZOMBIE_WALK.add(MACINNIS_ZOMBIE_02);
-        //carry on!!!!
+        MACINNIS_ZOMBIE_WALK.add(MACINNIS_ZOMBIE_03);
+        MACINNIS_ZOMBIE_WALK.add(MACINNIS_ZOMBIE_04);
+        MACINNIS_ZOMBIE_WALK.add(MACINNIS_ZOMBIE_05);
+        MACINNIS_ZOMBIE_WALK.add(MACINNIS_ZOMBIE_06);
+        MACINNIS_ZOMBIE_WALK.add(MACINNIS_ZOMBIE_07);
+        MACINNIS_ZOMBIE_WALK.add(MACINNIS_ZOMBIE_08);
+        MACINNIS_ZOMBIE_WALK.add(MACINNIS_ZOMBIE_09);
+        MACINNIS_ZOMBIE_WALK.add(MACINNIS_ZOMBIE_10);
+        MACINNIS_ZOMBIE_WALK.add(MACINNIS_ZOMBIE_11);
+        MACINNIS_ZOMBIE_WALK.add(MACINNIS_ZOMBIE_12);
+        MACINNIS_ZOMBIE_WALK.add(MACINNIS_ZOMBIE_13);
+        MACINNIS_ZOMBIE_WALK.add(MACINNIS_ZOMBIE_14);
+
 
         LAWRENCE_ZOMBIE_WALK = new ArrayList<>();
         LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_01);
-        //carry on!!!!
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_01);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_01);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_02);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_03);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_04);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_05);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_06);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_07);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_08);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_09);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_10);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_11);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_12);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_13);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_14);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_15);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_16);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_17);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_18);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_19);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_20);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_21);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_22);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_23);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_24);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_25);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_26);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_27);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_28);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_29);
+        LAWRENCE_ZOMBIE_WALK.add(LAWRENCE_ZOMBIE_30);
+
+        
+        PATEL_ZOMBIE_WALK = new ArrayList<>();
+        PATEL_ZOMBIE_WALK.add(PATEL_ZOMBIE_01);
+        PATEL_ZOMBIE_WALK.add(PATEL_ZOMBIE_02);
+        PATEL_ZOMBIE_WALK.add(PATEL_ZOMBIE_03);
+        PATEL_ZOMBIE_WALK.add(PATEL_ZOMBIE_04);
+        PATEL_ZOMBIE_WALK.add(PATEL_ZOMBIE_05);
+        PATEL_ZOMBIE_WALK.add(PATEL_ZOMBIE_06);
+        PATEL_ZOMBIE_WALK.add(PATEL_ZOMBIE_07);
+        PATEL_ZOMBIE_WALK.add(PATEL_ZOMBIE_08);
+        PATEL_ZOMBIE_WALK.add(PATEL_ZOMBIE_09);
+
+
+            
+
+
     }
 
     
